0c8b8605dd73bb202f02dc2c1eb9d15003d73eec
zhukovdigital <sashazh316@gmail.com>
2023-04-11 15:34:50 +0300

    init

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    extend: {},
+    fontFamily: {
+      sans: ["Clash Grotesk", "sans-serif"],
+    },
+  },
+  plugins: [],
+};
